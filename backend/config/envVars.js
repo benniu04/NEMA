9d2d9626dac410946ec4da8089e9a13f1536fcd6
benniu04 <nub38bn@gmail.com>
2025-06-01 23:51:12 -0400

    api url change

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -15,5 +15,4 @@ export const ENV_VARS = {
     AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
     AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
     AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
-    API_URL: process.env.VITE_API_URL
 }
